7890ee74916bb4a26eb8833f0c7ececf6b9f6533
AustinHoover1218 <ahoover7@vols.utk.edu>
2020-05-29 16:41:25 -0400

    Finished 5.9 and 5.10.

--- epi_judge_python/prime_sieve.py
+++ epi_judge_python/prime_sieve.py
@@ -2,11 +2,16 @@ from typing import List
 
 from test_framework import generic_test
 
-
-# Given n, return all primes up to and including n.
 def generate_primes(n: int) -> List[int]:
-    # TODO - you fill in here.
-    return []
+    primes = []
+    is_prime = [False, False] + [True] * (n-1)
+    for p in range(2, n+1):
+        if is_prime[p]:
+            primes.append(p)
+            # Remove all multiples of p
+            for i in range(p, n+1, p):
+                is_prime[i] = False
+    return primes
 
 
 if __name__ == '__main__':
--- epi_judge_python_solutions/apply_permutation.py
+++ epi_judge_python_solutions/apply_permutation.py
@@ -4,7 +4,6 @@ from test_framework import generic_test
 
 
 def apply_permutation(perm: List[int], A: List[int]) -> None:
-
     for i in range(len(A)):
         while perm[i] != i:
             A[perm[i]], A[i] = A[i], A[perm[i]]
