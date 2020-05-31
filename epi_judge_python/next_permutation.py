68dcf1a5581d358063bc8475e0df96ee479e7513
AustinHoover1218 <ahoover7@vols.utk.edu>
2020-05-31 14:39:23 -0400

    Polished answer for 5.11.

--- epi_judge_python/next_permutation.py
+++ epi_judge_python/next_permutation.py
@@ -5,31 +5,23 @@ from test_framework import generic_test
                                                                      #
 def next_permutation(perm: List[int]) -> List[int]:
 
-    # Check for array of length 1
-    n = len(perm)
-    if n < 2:
+    # Travel backwards until we find perm[i] < perm[i+1]
+    i = len(perm) - 2
+    while i >= 0 and perm[i] >= perm[i + 1]:
+        i -= 1
+    if i < 0:  # last permutation
         return []
 
-    # Try to swap last two elements
-    if perm[-1] > perm[-2]:
-        perm[-1], perm[-2] = perm[-2], perm[-1]
-    else:
-        # Travel backwards until we find perm[i] < perm[i+1]
-        i = n - 2
-        while perm[i] >= perm[i + 1]:
-            i -= 1
-            if i < 0:  # last permutation
-                return []
-        # Travel backwards from end of array to find smallest
-        # element in perm[i+1:] larger than perm[i], then swap
-        # it with perm[i].
-        for j in reversed(range(i+1, n)):
-            if perm[j] > perm[i]:
-                perm[j], perm[i] = perm[i], perm[j]
-                break
-        # perm[i+1:] is already sorted in descending order,
-        # but it now needs to be in ascending order.
-        perm[i+1:] = reversed(perm[i+1:])
+    # Travel backwards from end of array to find smallest
+    # element in perm[i+1:] larger than perm[i], then swap
+    # it with perm[i].
+    for j in reversed(range(i+1, len(perm))):
+        if perm[j] > perm[i]:
+            perm[j], perm[i] = perm[i], perm[j]
+            break
+    # perm[i+1:] is already sorted in descending order,
+    # but it now needs to be in ascending order.
+    perm[i+1:] = reversed(perm[i+1:])
     return perm
 
 if __name__ == '__main__':
