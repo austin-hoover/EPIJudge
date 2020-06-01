44c0ffa219afaa14269f39e6a3afe6ba1ca341e2
AustinHoover1218 <ahoover7@vols.utk.edu>
2020-06-01 09:49:38 -0400

    Finished 5.12.

--- epi_judge_python/offline_sampling.py
+++ epi_judge_python/offline_sampling.py
@@ -9,8 +9,10 @@ from test_framework.test_utils import enable_executor_hook
 
 
 def random_sampling(k: int, A: List[int]) -> None:
-    # TODO - you fill in here.
-    return
+    import random
+    for i in range(k):
+        r = random.randint(i, len(A)-1)
+        A[i], A[r] = A[r], A[i]
 
 
 @enable_executor_hook
