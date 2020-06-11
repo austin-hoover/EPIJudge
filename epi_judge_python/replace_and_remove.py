253c19fcd64b3c342b6016dd1f86e8314c32e79d
AustinHoover1218 <ahoover7@vols.utk.edu>
2020-06-11 11:46:16 -0400

    Finished 6.4.

--- epi_judge_python/replace_and_remove.py
+++ epi_judge_python/replace_and_remove.py
@@ -6,8 +6,26 @@ from test_framework.test_utils import enable_executor_hook
 
 
 def replace_and_remove(size: int, s: List[str]) -> int:
-    # TODO - you fill in here.
-    return 0
+
+    # Remove b's
+    for i in range(size):
+        while s[i] == 'b':
+            for j in range(i, size):
+                s[j] = s[j + 1]
+
+    # Convert a's to d's
+    n_valid = len(s) - s.count('')
+    size = n_valid + s.count('a')
+    write_idx = size - 1
+    for char in reversed(s[:n_valid]):
+        if char == 'a':
+            s[write_idx] = s[write_idx - 1] = 'd'
+            write_idx -= 2
+        else:
+            s[write_idx] = char
+            write_idx -= 1
+
+    return size
 
 
 @enable_executor_hook
